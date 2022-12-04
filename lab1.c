7d1e92b4923c9b3dc27b63f24f3851ede567b07f
ennaivat <77747268+ennaivat@users.noreply.github.com>
2022-12-03 22:08:46 -0600

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -1,5 +1,25 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "lab1.h"
+
+
+char*readString(char*file){
+  char*ptr = malloc(MAX_LINE_LEN*sizeof(char));
+    FILE*txt = fopen(file,"r";
+strcpy(ptr,fgets(ptr,MAX_LINE_LEN-1,txt));
+                     fclose(txt);
+                     rerturn ptr;
+                     }
+                     
+char*mysteryExplode(const char*str){
+  int len = strlen(str) -1;
+  char*newStr = calloc(((len*(len+1))/2),1);
+  for(int i = 0; i < len; i++){
+    for(int j = 0; j < i + 1; j++){
+      strncat(newStr, &str[j],1);
+    }
+  }
+  return newStr;
+}
 
